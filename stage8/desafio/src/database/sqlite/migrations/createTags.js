dedf2c0ca053b822687690118af05ad7d59ed6dc
hanyelC <hanyel.chamon@gmail.com>
2022-05-22 13:09:02 -0300

    add foreign keys and cascade deleting to DB

--- stage8/desafio/src/database/sqlite/migrations/createTags.js
+++ stage8/desafio/src/database/sqlite/migrations/createTags.js
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS tags (
   note_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
   name VARCHAR NOT NULL,
-  FOREIGN KEY(note_id) REFERENCES notes(id),
-  FOREIGN KEY(user_id) REFERENCES users(id)
+  FOREIGN KEY (note_id) REFERENCES notes(id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 `
 
--- stage8/desafio/src/server.js
+++ stage8/desafio/src/server.js
@@ -11,8 +11,5 @@ app.use(express.json())
 
 app.use(routes)
 
-// app.use('/', (req, res) => { 
-//   return res.send('Hello from server')
-// })
 
 app.listen(3030, console.log('server running on port 3030'))

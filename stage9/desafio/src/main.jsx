5de0a4d01e43974d8002a02e9db14ffce0b89d01
hanyelC <hanyel.chamon@gmail.com>
2022-06-04 17:28:24 -0300

    Details page created

--- stage9/desafio/src/main.jsx
+++ stage9/desafio/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from "styled-components"
 import GlobalStyle from "./styles/global"
 
-import { Home } from "./pages/Home"
+import { Details } from "./pages/Details"
 
 import { theme } from "./styles/theme"
 
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      <Details />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ stage9/desafio/src/pages/Details/styles.js
@@ -0,0 +1,82 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: auto auto;
+  grid-template-areas:
+  "header"
+  "content" ;
+
+  overflow-y: hidden;
+`
+
+export const Content = styled.div`
+  grid-area: content;
+
+  display: grid;
+  gap: 4rem;
+  margin: 4rem 11.5rem;
+
+  text-align: justify;
+
+  overflow-y: auto;
+  
+  & > header {
+    display: flex;
+    flex-direction: column;
+    gap: 2.4rem;
+    align-items: flex-start;
+
+    & > a {
+      margin-top: 0;
+    }
+  }
+  
+  & > header > div:first-of-type {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+  }
+
+  & h2 {
+    font-weight: 500;
+    font-size: 3.6rem;
+    line-height: 4.8rem;
+  }
+`
+
+export const Rating = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  & > svg {
+    font-size: 2rem;
+    color: ${({ theme}) => theme.COLORS.PINK}
+  }
+`
+
+export const Info = styled.div`
+  display: flex;
+  gap: 0.8rem;
+
+  align-items: center;
+
+  font-size: 1.6rem;
+
+  & > img {
+    height: 1.6rem;
+    width: 1.6rem;
+    border-radius: 50%;
+  }
+
+  & > svg {
+    color: ${({ theme}) => theme.COLORS.PINK}
+  }
+`
+
+export const Tags = styled.div``
+
+export const Description = styled.div`
+`

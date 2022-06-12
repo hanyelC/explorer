1fd1f63606421e64f0bc70f5345ecab424cfcdd9
hanyelC <hanyel.chamon@gmail.com>
2022-06-12 11:05:35 -0300

    cors

--- stage8/api/src/controllers/UsersController.js
+++ stage8/api/src/controllers/UsersController.js
@@ -44,7 +44,7 @@ class UsersController {
     }
     
     if(password && !old_password){
-      throw new AppError("Você precisa informar a senha antiga para definir as nova senha")
+      throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
     }
 
     if(password && old_password){

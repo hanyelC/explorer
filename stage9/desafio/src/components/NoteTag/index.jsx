7156fbba2f37b0be88c8cb9d5179ab57212d9fff
hanyelC <hanyel.chamon@gmail.com>
2022-07-08 17:13:09 -0300

    Feat: Note tag component

--- stage9/desafio/src/components/NoteTag/index.jsx
+++ stage9/desafio/src/components/NoteTag/index.jsx
@@ -1,9 +1,25 @@
+import { FiPlus, FiX } from "react-icons/fi"
+
 import { Container } from "./styles"
 
-export function NoteTag() {
-  return (
-    <Container>
+export function NoteTag({isNew, value, placeholder, onClick, ...rest}) {
 
+  return (
+    <Container isNew={isNew}>
+      <input
+        type="text"
+        readOnly={!isNew}
+        value={value}
+        {...rest}
+        placeholder={isNew ? placeholder ? placeholder : "Novo" : ""}
+      />
+      
+      <button
+        type="button"
+        onClick={onClick}
+      >
+        {isNew ? <FiPlus /> : <FiX />}
+      </button>
     </Container>
   )
 }

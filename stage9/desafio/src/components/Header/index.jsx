18e68ce079974fe48cb2e3510726bd45000fcbab
hanyelC <hanyel.chamon@gmail.com>
2022-07-11 14:28:06 -0300

    feat: using avatar image given from backend

--- stage9/desafio/src/components/Header/index.jsx
+++ stage9/desafio/src/components/Header/index.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom"
 
+import { useAuth } from "../../hooks/auth"
+import { api } from "../../services/api"
+
 import avatarPlaceholder from "../../assets/avatar_placeholder.svg"
 
 import { Input } from "../Input"
 
 import { Container } from "./styles"
-import { useAuth } from "../../hooks/auth"
 
 export function Header({ onSearch }) {
   const { signOut, user } = useAuth()
 
-  const avatarUrl = user.avatar ? user.avatar : avatarPlaceholder
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
 
   function handleSignOut() {
     if (confirm("Deseja realmente sair?"))
@@ -24,20 +26,20 @@ export function Header({ onSearch }) {
 
       {
         onSearch
-        ? <Input
+          ? <Input
             placeholder="Pesquisar pelo título"
             onChange={(e) => onSearch(e.target.value)}
           />
-        : <Input placeholder="Pesquisar pelo título"/>
+          : <Input placeholder="Pesquisar pelo título" />
       }
-      
+
 
       <div>
         <div>
           <p>{user.name}</p>
           <button onClick={handleSignOut}>sair</button>
         </div>
-        <Link to="/profile"><img src={avatarUrl} alt="" /></Link>
+        <Link to="/profile"><img src={avatarUrl} alt="Imagem de perfil do usuário" /></Link>
       </div>
 
     </Container>

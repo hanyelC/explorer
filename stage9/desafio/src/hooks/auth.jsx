b2c1cd96c279d06445f3d22fd84f81b76af38608
hanyelC <hanyel.chamon@gmail.com>
2022-07-11 07:14:19 -0300

    style: better indentation

--- stage9/desafio/src/hooks/auth.jsx
+++ stage9/desafio/src/hooks/auth.jsx
@@ -1,56 +1,56 @@
-import { createContext, useContext, useState, useEffect} from "react"
+import { createContext, useContext, useState, useEffect } from "react"
 
 import { api } from "../services/api"
 
 const AuthContext = createContext({})
 
 export function AuthProvider({ children }) {
-  const [data, setData ] = useState({})
+  const [data, setData] = useState({})
 
-  async function signUp(name, email, password){
+  async function signUp(name, email, password) {
     try {
-      await api.post("/users",{ name, email, password})
+      await api.post("/users", { name, email, password })
     } catch (error) {
       console.log(error)
-      if(error.response)
+      if (error.response)
         alert(error.response.data.message)
     }
   }
-  
-  async function signIn(email, password){
+
+  async function signIn(email, password) {
 
     try {
-      const response = await api.post("/sessions",{email,password})
+      const response = await api.post("/sessions", { email, password })
       const { user, token } = response.data
 
       localStorage.setItem("@rocketmovies:user", JSON.stringify(user))
       localStorage.setItem("@rocketmovies:token", token)
-      
+
       api.defaults.headers.authorization = `Bearer ${token}`
 
-      setData({ user, token})
-      
-    } catch(error) {
+      setData({ user, token })
+
+    } catch (error) {
       console.log(error)
-      if(error.response.data.message) alert(error.response.data.message)
+      if (error.response.data.message) alert(error.response.data.message)
       else alert("Algo deu errado")
     }
 
   }
 
-  function signOut(){
+  function signOut() {
     localStorage.removeItem("@rocketmovies:user")
     localStorage.removeItem("@rocketmovies:token")
     setData({})
   }
-  
-  useEffect(()=> {
+
+  useEffect(() => {
     const user = JSON.parse(localStorage.getItem("@rocketmovies:user"))
     const token = localStorage.getItem("@rocketmovies:token")
 
-    setData({ user, token})
+    setData({ user, token })
   }, [])
-  
+
   return (
 
     <AuthContext.Provider value={{
